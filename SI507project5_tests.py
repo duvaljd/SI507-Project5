5b259274be79b02d54a6bcfa6186d2ab44c1c981
jd <duvaljd@umich.edu>
2017-11-11 14:56:23 -0500

    updated test code

--- SI507project5_tests.py
+++ SI507project5_tests.py
@@ -5,15 +5,31 @@ from SI507project5_code import *
 
 
 
+class test_cacheFiles(unittest.TestCase):
+    def setUp(self):
+        self.data = open("cache_contents.json",'r')
+        self.creds = open("creds.json",'r')
 
+    def test_existence(self):
+        self.assertTrue(self.data.read())
+        self.assertTrue(self.creds.read())
 
+    def tearDown(self):
+        self.data.close()
+        self.creds.close()
 
+class test_csvFiles(unittest.TestCase):
+    def setUp(self):
+        self.colorful = open("colorful.csv",'r')
+        self.paintings = open("paintings.csv",'r')
 
+    def test_existence(self):
+        self.assertTrue(self.colorful.read())
+        self.assertTrue(self.paintings.read())
 
-
-
-
-
+    def tearDown(self):
+        self.colorful.close()
+        self.paintings.close()
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
